da80ff6b743d6d755261ca3b254d39c210893374
mohanrit <35220535+mohanrit@users.noreply.github.com>
2018-01-19 11:11:51 +0000

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+void main()
+{
+int j,a,sum=0;
+printf("enter the number");
+scanf("%d",&a);
+for(j=1;j<=a;j++)
+{
+sum=sum+j;
+}
+printf("sum of n natural numbers is");
+scanf("%d",&sum);
+}
+return 0;
+}
